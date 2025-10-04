c90756b07885a2878c3dde10c7d9a5163e94f7e1
FLAme_Z <fatihadani76@gmail.com>
2025-10-04 11:46:00 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     zenchainTestnet: {
       url: "https://zenchain-testnet.api.onfinality.io/public", // RPC endpoint
-      accounts: [ "b36cf887618f715ae14556b44a66a628ea8f90f78424999a8b68cb3e6452a9cb" ],
+      accounts: [ "" ],
       chainId: 8408,
       // Jangan share private key di publik, hanya untuk dev!
       // Bisa juga pakai env var: accounts: [process.env.PRIVATE_KEY]
